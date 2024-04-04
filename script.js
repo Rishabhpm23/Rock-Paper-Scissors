18f5207a12c429ebdb71f0f4d58ba0e86b71041e
Rishabhpm23 <rxa6169@gmail.com>
2024-04-05 00:19:28 +0530

    add css

--- script.js
+++ script.js
@@ -6,42 +6,76 @@ function getComputerChoice(){
 let playerScore = 0;
 let computerScore = 0;
 function playRound(playerSelection){
+    result.innerHTML = '';
     let computerSelection = getComputerChoice();
     if (playerSelection === "rock"){
         if(computerSelection === "rock"){
             //console.log("Tie!");
-            const score = document.getElementById('score');
-            score.textContent = 'Round Tied';
-            
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'Round Tied';
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         } else if(computerSelection === "paper"){
-            console.log("You Lose! Paper beats Rock");
+            //console.log("You Lose! Paper beats Rock");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'You Lose! Paper beats Rock';
             computerScore++;
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         } else{
-            console.log("You Win! Rock beats Scissors");
+            //console.log("You Win! Rock beats Scissors");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'You Win! Rock beats Scissors';
             playerScore++;
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         }
     } else if (playerSelection === "paper"){
         if(computerSelection === "paper"){
-            console.log("Tie!");
+            //console.log("Tie!");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'Round Tied';
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         } else if(computerSelection === "scissors"){
-            console.log("You Lose! Scissors beats Paper");
-            computerScore++
+            //console.log("You Lose! Scissors beats Paper");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'You Lose! Scissors beats Paper';
+            computerScore++;
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         } else{
-            console.log("You Win! Paper beats Rock");
+            //console.log("You Win! Paper beats Rock");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'You Win! Paper beats Rock';
             playerScore++;
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         }
     } else{
         if(computerSelection === "scissors"){
-            console.log("Tie!");
+            //console.log("Tie!");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'Round Tied';
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         } else if(computerSelection === "rock"){
-            console.log("You Lose! Rock beats Scissors");
+            //console.log("You Lose! Rock beats Scissors");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'You Lose! Rock beats Scissors';
             computerScore++;
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         } else{
-            console.log("You Win! Scissors beats Paper");
+            //console.log("You Win! Scissors beats Paper");
+            const score = document.querySelector('#score');
+            const points = document.querySelector('#points');
+            score.innerHTML = 'You Win! Scissors beats Paper';
             playerScore++;
+            points.innerHTML = "Score = Player: " + playerScore + "\nComputer: " + computerScore ;
         }
     }
-    console.log("Score = Player: " + playerScore + "\tComputer: " + computerScore);
+    //console.log("Score = Player: " + playerScore + "\tComputer: " + computerScore);
     if(playerScore === 5 || computerScore === 5){
         endResult();
         playerScore = 0;
@@ -50,11 +84,17 @@ function playRound(playerSelection){
 }
 function endResult(){
     if(playerScore === computerScore){
-        console.log("Its a Tie!");
+        //console.log("Its a Tie!");
+        const result = document.querySelector('#result');
+        result.innerHTML = 'GAME TIED!!'
     } else if(playerScore > computerScore){
-        console.log("YOU WIN!");
+        //console.log("YOU WIN!");
+        const result = document.querySelector('#result');
+        result.innerHTML = 'YOU WON THE GAME!!'
     } else{
-        console.log("YOU LOSE!");
+        //console.log("YOU LOSE!");
+        const result = document.querySelector('#result');
+        result.innerHTML = 'YOU LOST THE GAME!!'
     }
 }
 
